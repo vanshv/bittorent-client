72eff9349f43146afacba7f314fa26f6da35587a
vanshv <vanshvardhan.singh@iiitb.ac.in>
2022-03-17 23:34:00 +0530

    did go excercise and added good resource

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"net"
+	"net/url"
+	"strconv"
 	"github.com/jackpal/bencode-go"
 )
 
@@ -16,6 +22,21 @@ type bencodeTorrent struct {
     Info     bencodeInfo `bencode:"info"`
 }
 
+type TorrentFile struct {
+    Announce    string
+    InfoHash    [20]byte
+    PieceHashes [][20]byte
+    PieceLength int
+    Length      int
+    Name        string
+}
+
+// Peer encodes connection information for a peer
+type Peer struct {
+    IP   net.IP
+    Port uint16
+}
+
 func Open(r io.Reader) (*bencodeTorrent, error) {
     bto := bencodeTorrent{}
     err := bencode.Unmarshal(r, &bto)
@@ -23,4 +44,40 @@ func Open(r io.Reader) (*bencodeTorrent, error) {
         return nil, err
     }
     return &bto, nil
+}
+
+
+func (t *TorrentFile) buildTrackerURL(peerID [20]byte, port uint16) (string, error) {
+    base, err := url.Parse(t.Announce)
+    if err != nil {
+        return "", err
+    }
+    params := url.Values{
+        "info_hash":  []string{string(t.InfoHash[:])},
+        "peer_id":    []string{string(peerID[:])},
+        "port":       []string{strconv.Itoa(int(Port))},
+        "uploaded":   []string{"0"},
+        "downloaded": []string{"0"},
+        "compact":    []string{"1"},
+        "left":       []string{strconv.Itoa(t.Length)},
+    }
+    base.RawQuery = params.Encode()
+    return base.String(), nil
+}
+
+// Unmarshal parses peer IP addresses and ports from a buffer
+func Unmarshal(peersBin []byte) ([]Peer, error) {
+    const peerSize = 6 // 4 for IP, 2 for port
+    numPeers := len(peersBin) / peerSize
+    if len(peersBin)%peerSize != 0 {
+        err := fmt.Errorf("Received malformed peers")
+        return nil, err
+    }
+    peers := make([]Peer, numPeers)
+    for i := 0; i < numPeers; i++ {
+        offset := i * peerSize
+        peers[i].IP = net.IP(peersBin[offset : offset+4])
+        peers[i].Port = binary.BigEndian.Uint16(peersBin[offset+4 : offset+6])
+    }
+    return peers, nil
 }
